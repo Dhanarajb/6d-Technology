098ac94b66230e62ddf03029155684bdb1436976
Dhanarajb <88299676+Dhanarajb@users.noreply.github.com>
2023-12-21 19:18:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 6d-Technology
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/Dhanarajb/6d-Technology)
---
+++ index.js
@@ -0,0 +1,68 @@
+// 1. Child to parent
+// const ComponentA = () => {
+//   return <><ComponentB  /></>;
+// };
+// export default FunctionalComponent;
+
+// const ComponentB = ()=>{
+//   return <>world</>
+// }
+// export default ComponentB
+
+// 2. How to create ReactJS app
+// npx create-react-app DeviceMotionEvent
+// npm start
+// npm run build
+
+
+// 3. Component Composition
+// A.p B.ca C.cb
+// const CompA = () =>{
+//   return <>
+//.       content a
+//   </>
+// }
+// export default CompA
+
+// const CompB = ()=>{
+//   return <>
+//      <CompA >
+//   </>
+// }
+// export default CompB
+
+// const Compc =()=>{
+//   return <>
+//      <CompB />
+//   </>
+// }
+// export default CompC
+
+// const App = () => {
+//   return (
+//     <>
+//       <CompC />
+//     </>
+//   );
+// };
+
+// 4. How to create branch?
+// git branch component-name
+// component-name
+// git checkout -b component-name
+
+// 5. Life cyecle Methods
+// 6. Virtual DOM
+// 7. Sematic tags
+// 8. SCSS
+// 9. How to declare set how to add new
+// 10. what are the Classes in js
+// 11. Functional component vs class component
+// 12. what are the Fragments 
+// 13. useContext
+// 14. Custom template vs css library
+// 15. Usecase of resuable component in reactjs
+// 16. Microfrontend ?
+// 17. Padding vs margin
+// 18. what is GitHub?
+
